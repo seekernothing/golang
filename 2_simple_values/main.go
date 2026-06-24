3a64be7a1551d04ef246e1e249b0c8beafefff36
YourGitHubUsername <abhishekbiradar2856@gmail.com>
2026-06-24 21:08:17 +0530

    simple values in go

---
+++ 2_simple_values/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//int
+	fmt.Println(1 + 99)
+
+	//string
+	fmt.Println("This is a string")
+
+	// boolean
+
+	fmt.Println(true)
+	fmt.Println(false)
+
+	// float
+
+	fmt.Println(10.5)
+	fmt.Println(3.0/9.0)
+}
