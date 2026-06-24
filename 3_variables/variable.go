ee731699ed3529194a89be44234cc0c4e15c1221
YourGitHubUsername <abhishekbiradar2856@gmail.com>
2026-06-24 21:28:01 +0530

    varibles in go

---
+++ 3_variables/variable.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// string
+
+	var name = "string"
+
+	fmt.Println(name)
+
+	// bool
+
+	var isTrue = false
+
+	fmt.Println(isTrue)
+
+	//shorthand
+
+	Person := "Luffy"
+
+	fmt.Println(Person)
+
+	luckyNumer := 7
+
+	luckyFloat := 7.7
+
+	fmt.Println(luckyNumer)
+
+	fmt.Println("luccky float is", luckyFloat)
+
+}
